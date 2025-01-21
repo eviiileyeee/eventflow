7f5206aad99e8e4ffccff5dbb6e6e246e37711dc
harendra <jeaturajabundela@gmail.com>
2025-01-21 19:18:37 +0530

    Team members images updated

--- client/src/components/Team.jsx
+++ client/src/components/Team.jsx
@@ -17,24 +17,24 @@ const Team = () => (
         <h2 className="text-3xl font-bold text-center mb-12 text-gray-900 dark:text-white">Our Team</h2>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
           <TeamMember 
-            name="Alex Chen"
+            name="Anirudhh dugbe"
             role="Founder"
-            image="/api/placeholder/96/96"
+            image="https://media.licdn.com/dms/image/v2/D4D03AQHGEsPkhy8JjQ/profile-displayphoto-shrink_800_800/B4DZOwGadZHMAc-/0/1733826292465?e=1743033600&v=beta&t=THzZK0gxQrQOh3xl_rcYmyfq_wKAgYnlGshX2bXR9l0"
           />
           <TeamMember 
-            name="Sarah Johnson"
+            name="Atharva Thakur"
             role="Tech Lead"
-            image="/api/placeholder/96/96"
+            image="https://media.licdn.com/dms/image/v2/D4D03AQHSrkgQIZ-YiQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1712920335349?e=1743033600&v=beta&t=5ofGyE_iPzw2EA1Sdh4n_JRVlaJlkRGp06OOHZZ-7S8"
           />
           <TeamMember 
-            name="Michael Lee"
+            name="Anam Mansoori"
             role="Community Manager"
-            image="/api/placeholder/96/96"
+            image="https://media.licdn.com/dms/image/v2/D5603AQGNS_70VvB-DQ/profile-displayphoto-shrink_800_800/B56ZPjRx9HGQAc-/0/1734684912972?e=1743033600&v=beta&t=HhAssjPLfUe2uR03C4IRMGcPp4WOSlygR6mUvCoprAQ"
           />
           <TeamMember 
-            name="Emma Wilson"
+            name="Jigyarth Sharma"
             role="Events Coordinator"
-            image="/api/placeholder/96/96"
+            image="https://media.licdn.com/dms/image/v2/D5603AQF8STylHbMDLQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1726598317856?e=1743033600&v=beta&t=PJUxy2RqTtGfEUETxd3aGvJWfIm2TSRuYuSpHDK5HGU"
           />
         </div>
       </div>
