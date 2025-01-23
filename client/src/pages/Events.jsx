a90f02c3e120c5457595b8a3a53db07cfecb422d
Ayesha qureshi <qureshiayesha660@gmail.com>
2025-01-23 18:12:13 +0530

    teammm

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -146,7 +146,8 @@ const Events = () => {
   };
 
   return (
-    <div className=" min-h-screen bg-gradient-to-br from-white to-gray-100 dark:bg-gradient-to-br dark:from-gray-900 dark:via-gray-800 dark:to-black  p-8">
+    <div className=" min-h-screen bg-gradient-to-br from-[#91A5CA] via-[#C8CDD4] to-[#91A5CA]
+     dark:bg-gradient-to-br dark:from-gray-900 dark:via-gray-800 dark:to-black  p-8">
       <div className=" relative top-10 max-w-6xl mx-auto">
         <h1 className="text-3xl font-bold text-gray-800 dark:text-white mb-8">
           Events
