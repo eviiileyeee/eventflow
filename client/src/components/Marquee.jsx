75a044ad00a9aa942274ea75ff8978dac09da957
Ayesha qureshi <qureshiayesha660@gmail.com>
2025-01-23 23:31:41 +0530

    agdam bagdam

--- client/src/components/Marquee.jsx
+++ client/src/components/Marquee.jsx
@@ -23,10 +23,15 @@ const brands = [
 
 const MarqueeComponent = () => {
   const { darkMode } = useTheme();
+
   return (
-    <div className="bg-gradient-to-br from-[#91A5CA] via-[#AAB7CE] to-[#C6CBD3]
-     dark:bg-gradient-to-tb dark:from-[#101726] dark:via-[#090E16] dark:to-[#020304]
-      py-4">
+    <div
+      className={`py-4 ${
+        darkMode
+          ? "dark:bg-gradient-to-bl dark:from-[#020305] dark:via-[#05070B] dark:via-[#0B0F19] dark:to-[#121928]"
+          : "bg-gradient-to-t from-[#101726] via-[#1D2036] via-[#0F121C] to-[#010202]"
+      }`}
+    >
       <Marquee
         velocity={20}
         direction="rtl"
@@ -38,12 +43,14 @@ const MarqueeComponent = () => {
         {brands.concat(brands).map((brand, index) => {
           const IconComponent = brand.icon;
           return (
-            <div 
-              key={index} 
+            <div
+              key={index}
               className="px-8 mx-4 transition-all duration-300 hover:scale-110 hover:text-[#F0F0F0]"
             >
-              <IconComponent 
-                className={`h-8 w-8 text-[#6A6E77] ${darkMode ? "text-white" : ""}`} 
+              <IconComponent
+                className={`h-8 w-8 text-[#6A6E77] ${
+                  darkMode ? "text-white" : ""
+                }`}
               />
             </div>
           );
