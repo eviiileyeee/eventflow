5e502ced66d9e417eef9ededaac58bbcd6419838
harendra <jeaturajabundela@gmail.com>
2025-01-26 18:32:27 +0530

    deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 
-const API_URL =  'http://localhost:8001';
-
+const API_URL = import.meta.env.REACT_APP_API_URL;
 const api = axios.create({
   baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
 
 // Request interceptor
 api.interceptors.request.use(
