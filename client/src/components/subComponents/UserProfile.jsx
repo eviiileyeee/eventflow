c84858d741b8c8d354990be1ccde515752e24ed5
harendra <jeaturajabundela@gmail.com>
2025-02-15 13:05:21 +0530

    displaying backend data on UI profile

--- client/src/components/subComponents/UserProfile.jsx
+++ client/src/components/subComponents/UserProfile.jsx
@@ -7,6 +7,7 @@ import {
 export default function UserProfile(props) {
     const [isDarkMode, setIsDarkMode] = useState(false);
     const [openMenu, setopenMenu] = useState(false);
+    console.log(props.user);
     useEffect(() => {
 
         if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
@@ -20,7 +21,7 @@ export default function UserProfile(props) {
                 <div className="md:w-1/3 text-center mb-8 md:mb-0">
                     <img src={props.user.profileImage} alt="Profile" className="rounded-full object-cover w-48 h-48 mx-auto mb-4 border-4 border-indigo-800 dark:border-blue-900 transition-transform duration-300 hover:scale-105" />
                     <h1 className="text-2xl font-bold text-indigo-800 dark:text-white mb-2">{props.user.username}</h1>
-                    <p className="text-gray-600 dark:text-gray-300">Software Developer</p>
+                    <p className="text-gray-600 dark:text-gray-300">{props.user.profession}</p>
                     <button
                         className="mt-4 bg-indigo-800 text-white px-4 py-2 rounded-lg hover:bg-blue-900 transition-colors duration-300"
                         onClick={() => props.setTab("settings")}
@@ -30,7 +31,7 @@ export default function UserProfile(props) {
                 <div className="md:w-2/3 md:pl-8">
                     <h2 className="text-xl font-semibold text-indigo-800 dark:text-white mb-4">About Me</h2>
                     <p className="text-gray-700 dark:text-gray-300 mb-6">
-                        Passionate software developer with 5 years of experience in web technologies. I love creating user-friendly applications and solving complex problems.
+                        {props.user.discription }
                     </p>
                     <button
                         className="flex overflow-hidden items-center text-sm font-medium focus-visible:outline-none h-7
